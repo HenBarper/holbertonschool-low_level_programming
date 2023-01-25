722478115d08923b1fdc2a457691b4e64d64c471
HenBarper <Benjaminitonio@gmail.com>
2023-01-25 09:34:17 -0800

    4 v3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
+/**
+ * print message using 'puts'
+ *
+ * return 0
+ *
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzel");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
