4b1172a0bdaaf24dcbab8ed1468045569b96aa64
HenBarper <Benjaminitonio@gmail.com>
2023-01-25 12:05:03 -0800

    2 v1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+/*
+ * File 2-print_alphabet.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+
+/**
+ * main - I sometimes suffer from insomnia
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for(ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+
+	return (0);
+}
