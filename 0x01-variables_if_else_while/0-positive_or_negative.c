3780076471518ee466cfc23c446162f2c889662c
HenBarper <Benjaminitonio@gmail.com>
2023-01-25 11:16:55 -0800

    1 v3

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,11 +9,11 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n == 0)
+	if (n == 0)
 	{
 		printf("is zero\n");
 	}
-	else if(n % 2 == 0)
+	else if (n % 2 == 0)
 	{
 		printf("is positive\n");
 	}
