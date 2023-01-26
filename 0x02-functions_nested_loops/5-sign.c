bbc7c117382526bc4e9c62fa0a1691d24d751e3a
HenBarper <Benjaminitonio@gmail.com>
2023-01-26 11:45:22 -0800

    5 v1

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+/*
+ * File 5-isalpha.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - print sign of number
+ * @c: an input character
+ * Description: print sign
+ * Return: 1, 0, or -1
+ */
+
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		return (1);
+		printf("+");
+	}
+	else if (c == 0)
+	{
+		return (0);
+		printf("0");
+	}
+	else
+	{
+		return (-1);
+		printf("-");
+	}
+}
