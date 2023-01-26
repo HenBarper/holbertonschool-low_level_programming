79228787cf62a57fa5dcccc98f4f17df744064a4
HenBarper <Benjaminitonio@gmail.com>
2023-01-26 12:11:58 -0800

    7 v1

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+/*
+ * File 7-print_last_digit.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - print last dig of num
+ * @n: a variable number
+ * Description: last digit
+ * Return: Always 0
+ */
+
+int print_last_digit(int n)
+{
+	_putchar(n % 10);
+}
