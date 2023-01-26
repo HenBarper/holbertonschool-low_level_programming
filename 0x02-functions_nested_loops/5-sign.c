cb68ffe380c19d2d9342c04f9db273b1fb1bcf82
HenBarper <Benjaminitonio@gmail.com>
2023-01-26 11:52:28 -0800

    5 v4

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,21 +13,21 @@
  * Return: 1, 0, or -1
  */
 
-int print_sign(int c)
+int print_sign(int n)
 {
-	if (c > 0)
+	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
-	else if (c == 0)
+	else if (n == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar("-");
+		_putchar('-');
 		return (-1);
 	}
 }
