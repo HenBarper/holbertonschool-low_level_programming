b1684747449ab84b3cc87afaa3d0fa34e02ffcdc
HenBarper <Benjaminitonio@gmail.com>
2023-01-26 12:52:28 -0800

    8 v1

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,33 @@
+/*
+ * File 8-24_hours.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - print clock minutes
+ * @n: a variable number
+ * Description: clock
+ * Return: Always 0
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0 ; hour < 24 ; hour++)
+	{
+		for (minute = 0 ; minute < 60 ; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
