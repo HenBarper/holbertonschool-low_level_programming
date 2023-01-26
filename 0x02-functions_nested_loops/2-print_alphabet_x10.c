dc0083904ae2097e33940df236c83df281061d4a
HenBarper <Benjaminitonio@gmail.com>
2023-01-26 11:03:49 -0800

    2 v1

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+/*
+ * File 2-print_alphabet_x10.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - 10x alphabet
+ * Description: Prints alphabet ten times then new line
+ * Return: Nothing.
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i = 1;
+
+	while (i <= 10)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
