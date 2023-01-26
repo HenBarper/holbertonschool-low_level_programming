32319314fe2ea3ae95adcf5bdcd2fef22109b72a
HenBarper <Benjaminitonio@gmail.com>
2023-01-26 09:29:49 -0800

    0 v1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+ * File 0-putchar.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+
+/**
+ * main - _putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
