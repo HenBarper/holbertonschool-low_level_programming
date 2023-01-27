3848a0e20266d45fe9fc06bfeb9b2172ac3c72ff
HenBarper <Benjaminitonio@gmail.com>
2023-01-27 11:03:36 -0800

    10 v1

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,22 @@
+/*
+ * File 10-add.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * Description: add two numbers
+ * Return: Always 0
+ */
+
+int add(int a, int b);
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
