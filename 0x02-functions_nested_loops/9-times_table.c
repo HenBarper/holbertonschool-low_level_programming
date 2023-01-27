6776250343645fc574e721e937997fb46740e35e
HenBarper <Benjaminitonio@gmail.com>
2023-01-27 10:40:18 -0800

    9 v8

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,9 +18,9 @@ void times_table(void)
 	int num2;
 	int result;
 
-	for (num1 = '0' ; num1 <= '9' ; num1++)
+	for (num1 = 0 ; num1 <= 9 ; num1++)
 	{
-		for (num2 = '0' ; num2 <= '9' ; num2++)
+		for (num2 = 0 ; num2 <= 9 ; num2++)
 		{
 			result = num1 * num2;
 
