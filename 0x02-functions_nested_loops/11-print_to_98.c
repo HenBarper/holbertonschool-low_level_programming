611af982cb3610f9023556a4355f6ebdeb8d07a0
HenBarper <Benjaminitonio@gmail.com>
2023-01-27 11:18:54 -0800

    11 v1

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,45 @@
+/*
+ * File 11-print_to_98.c
+ * Auth: Ben Harper
+ */
+
+#include <stdio.h>
+#include <stdlib>
+#include "main.h"
+
+/**
+ * print_to_98 - printing to 98
+ * @n: input char
+ * Description: from n to 98 up or down
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	int c;
+
+	if (n < 99)
+	{
+		for ( c = n ; c <= 98 ; c++)
+		{
+			_putchar(c + '0');
+			if (c < 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for ( c = n ; c >= 98 ; c--)
+		{
+			_putchar(c + '0');
+			if(c > 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
