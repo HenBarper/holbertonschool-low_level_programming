d27183cab88e0d2dad29dd8fcc070dc0df056903
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 10:02:53 -0800

    3 v1

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * File 3-print_numbers.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * Description: print numbers 0 - 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar(i + '0');
+	}
+}
