c487612539c899c0eeb1e56425de1ae333e8a537
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 12:41:12 -0800

    8 v2

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -19,9 +19,9 @@ void print_square(int size)
 
 	if (n > 0)
 	{
-		for (i = 0 ; i < n ; i++)
+		for (i = 0 ; i < size ; i++)
 		{
-			for (j = 0 ; j < n ; j++)
+			for (j = 0 ; j < size ; j++)
 			{
 				_putchar('#');
 			}
