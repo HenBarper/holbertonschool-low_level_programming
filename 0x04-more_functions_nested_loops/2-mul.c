2167619eb27d06f73cf26a6fe8a3c9bc472e6106
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 09:59:20 -0800

    2 v1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File 2-mul.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * mul - times two digits
+ * @a: digit 1
+ * @b: digit 2
+ * Description: a * b
+ * Return: the product of a & b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
