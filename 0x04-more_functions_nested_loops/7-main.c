ae07adc74e7e5360a88148090eaf348c784b67d7
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 12:18:46 -0800

    7 v2

---
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - sahda
+ *
+ * Return: dkas
+ */
+int main(void)
+{
+	print_diagonal(0);
+	print_diagonal(2);
+	print_diagonal(10);
+	print_diagonal(-4);
+	return (0);
+}
