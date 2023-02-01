8e1a23adb98ed0a7c027b9f6fd361d38ae89b13e
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 12:40:15 -0800

    8 v1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,35 @@
+/*
+ * File 8-print_square
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * print_square - print squares of hashtags
+ * @size: input
+ * Description: use hashtags
+ * Return: 0
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j < n ; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
