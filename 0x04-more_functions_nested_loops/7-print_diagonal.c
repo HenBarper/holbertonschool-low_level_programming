7230288789863ff5447b22932db443fa074a1b01
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 12:11:39 -0800

    7 v1

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,32 @@
+/*
+ * File 7-print_diagonal.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * print_diagonal - print slashes diagonally
+ * @n: print line of length n
+ * Description: use slashes
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j < i ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+		}
+	}
+	_putchar('\n');
+}
