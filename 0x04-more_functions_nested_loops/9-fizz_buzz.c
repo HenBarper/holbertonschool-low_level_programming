40f0d85f592217d352d76ac8a95e1ef1a7f0eee3
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 12:54:07 -0800

    9 v1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+/*
+ * File 9-fizz_buzz
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * fizz_buzz - fizz buzz
+ * Description: Fizz - 3 _ Buzz - 5 _ FizzBuzz 5 & 3
+ * Return: nothing
+ */
+
+void fizz_buzz(void)
+{
+	int counter;
+
+	for (counter = 1 ; counter <= 100 ; counter++)
+	{
+		if ((counter % 3 == 0) && (counter % 5 == 0))
+		{
+			printf("FizzBuzz");
+		}
+		else if (counter % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (counter % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", counter);
+		}
+	}
+}
