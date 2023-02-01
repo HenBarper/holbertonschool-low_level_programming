6fc745028b7265651cddf5cb45cfc336113d5710
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 10:21:31 -0800

    3 v5

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -13,11 +13,11 @@
 
 void print_numbers(void)
 {
-	int i = '0';
+	int i;
 
-	for (i = '0' ; i <= '9' ; i++)
+	for (i = 0 ; i <= 9 ; i++)
 	{
-		_putchar(i);
+		_putchar(i + '0');
 	}
 	_putchar('\n');
 }
