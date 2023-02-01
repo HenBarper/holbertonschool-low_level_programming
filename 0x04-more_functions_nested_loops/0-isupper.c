f02222883d1cad078e9c91766d59d2b65fb9d733
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 09:51:37 -0800

    0 v1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,25 @@
+/*
+ * File 0-isupper.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * _isupper - check is upper
+ * @c: input char
+ * Description: check if char is upper case
+ * Return: 1 if upper, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if(c > 64 && c < 91)
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+int _isupper(int c);
+#endif
