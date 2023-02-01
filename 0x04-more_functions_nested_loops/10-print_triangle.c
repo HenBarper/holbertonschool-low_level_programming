49cfbf1f40191e527c32f0b775bfac193b30264a
HenBarper <Benjaminitonio@gmail.com>
2023-02-01 13:16:37 -0800

    10 v2

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,30 +6,30 @@
 #include "main.h"
 
 /**
- * print_triangle - 
+ * print_triangle - something
  * @size: input
  * Description: use hashtags
  * Return: nothing
  */
 
-void print_triangle(int n)
+void print_triangle(int size)
 {
 	int row;
 	int column;
 	int spaces;
 	int hTags;
 
-	if (n > 0)
+	if (size > 0)
 	{
-		for (row = 1 ; row <= n ; row++)
+		for (row = 1 ; row <= size ; row++)
 		{
-			for (column = 1 ; column <= n ; column++)
+			for (column = 1 ; column <= size ; column++)
 			{
-				for (spaces = (n - column) ; spaces > 0 ; spaces--)
+				for (spaces = (size - column) ; spaces > 0 ; spaces--)
 				{
 					_putchar(' ');
 				}
-				for (hTags = 1 ; hTags <= n ; hTags++)
+				for (hTags = 1 ; hTags <= size ; hTags++)
 				{
 					_putchar('#');
 				}
