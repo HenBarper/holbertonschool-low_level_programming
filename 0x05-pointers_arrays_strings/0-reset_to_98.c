b5fff1734c40cb6d030f02f24feab39fe8b450cc
HenBarper <Benjaminitonio@gmail.com>
2023-02-02 10:38:04 -0800

    0x05 first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+/*
+ * File 0-reset_to_98.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - reset param to 98
+ * @n: input pointer
+ * Return: nothing it's void
+ */
+
+void reset_to_98(int *n)
+{
+
+}
