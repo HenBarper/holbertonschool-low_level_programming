c1db327fb65db964affb61522ceb6e283828e6c9
HenBarper <Benjaminitonio@gmail.com>
2023-02-02 11:28:09 -0800

    3 v1

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+/*
+ * File 3-puts.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * _puts - print string + new line to "stdout"
+ * @str: strin value
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int length = _strlen(str);
+	int i;
+
+	for (i = 0 ; i < length ; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
