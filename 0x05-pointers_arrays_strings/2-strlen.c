da7f0030d9135b7ecc7c208dabb2c6bdeb9be5c8
HenBarper <Benjaminitonio@gmail.com>
2023-02-02 10:51:21 -0800

    2 v1

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+/*
+ * File 2-strlen.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * _strlen - return string length
+ * @s: char value
+ * Return: string length
+ */
+
+int _strlen(char *s);
+{
+	strlen(*s);
+}
