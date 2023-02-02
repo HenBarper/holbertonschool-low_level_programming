889b86369a107a3e3ba7b5ae0469efa56c1d98a2
HenBarper <Benjaminitonio@gmail.com>
2023-02-02 12:06:41 -0800

    4 v1

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+/*
+ * File 4-print_rev.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse + new line
+ * @s: string value
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i;
+	int length - _strlen(s);
+
+	for (i = length ; i > 0 ; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
