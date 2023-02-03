e10b2cdaee83b1ed6f31c671b877d70ed5f7b2ff
HenBarper <Benjaminitonio@gmail.com>
2023-02-03 11:02:13 -0800

    8 v1

---
+++ 0x05-pointers_arrays_strings/8.print_array.c
@@ -0,0 +1,40 @@
+/*
+ * File 8-print_array.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * puts_array - print n elements of a string + new line to "stdout"
+ * @n: input int
+ * Return: nothing
+ */
+
+void print_array(int *a, int n)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	if (n < length)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			printf("%d", a[i]);
+			if (i < (n - 1))
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		break;
+	}
+	_putchar('\n');
+}
