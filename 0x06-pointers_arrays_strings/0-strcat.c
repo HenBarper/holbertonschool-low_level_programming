9881c642882ad638812c67eb25fd009c014c982e
HenBarper <Benjaminitonio@gmail.com>
2023-02-08 09:37:26 -0800

    0 v1

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,4 +14,17 @@
 
 char *_strcat(char *dest, char *src)
 {
+	int dest_length;
+	int src_length;
+
+	while ( dest[dest_length] != '\0')
+	{
+		dest_length++;
+	}
+
+	for ( src_length = 0 ; src[src_length] != '\0' ; src_length++)
+	{
+		dest[dest_length] = src[src_length];
+		dest_length++;
+	}
 }
