bbc2a2dec1bc951eaa743571566c0c6799ce6888
HenBarper <Benjaminitonio@gmail.com>
2023-02-08 11:04:02 -0800

    3 v5

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,33 +14,5 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int s1Length = 0;
-	int s2Length = 0;
-
-	while (s1[s1Length] != '\0')
-	{
-		s1Length++;
-	}
-	s1Length--;
-
-	while (s2[s2Length] != '\0')
-	{
-		s2Length++;
-	}
-	s2Length--;
-
-	return ( *s1 - *s2);
-
-	if (s1Length < s2Length)
-	{
-		return (-15);
-	}
-	else if (s1Length == s2Length)
-	{
-		return (0);
-	}
-	else
-	{
-		return (15);
-	}
+	return (*s1 - *s2);
 }
