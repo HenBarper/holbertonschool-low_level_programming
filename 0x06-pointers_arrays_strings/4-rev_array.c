1ce06d4ab05e988607c694493e1a0aa093d56ba3
HenBarper <Benjaminitonio@gmail.com>
2023-02-08 12:51:55 -0800

    4 v1

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,27 @@
+/*
+ * File 4-rev_array.c
+ * Auth: Ben Harper
+ */
+
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse array of ints
+ * @a: int array
+ * @n: number or ints in array
+ * Return: nothing
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int counter;
+
+	for (counter = 0 ; counter < n ; counter ++)
+	{
+		temp = a[n-1];
+		a[n-1] = a[counter];
+		a[counter] = temp;
+		n--;
+	}
+}
