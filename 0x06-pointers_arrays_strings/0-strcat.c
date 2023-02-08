da07dc71df03b0f35063244fe158031d6d880e86
HenBarper <Benjaminitonio@gmail.com>
2023-02-08 09:24:34 -0800

    0x06 set-up

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+/*
+ * File 0-strcat.c
+ * Auth: Ben Harper
+ */
+
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenate two strings
+ * @*dest: value 1
+ * @*src: value 2
+ * Return: pointer to dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include<stdio.h>
+
+int _putchar(char c);
+
+char *_strcat(char *dest, char *src);
+#endif
