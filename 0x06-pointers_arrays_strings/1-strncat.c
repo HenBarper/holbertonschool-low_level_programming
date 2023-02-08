1824505ffd9cf15e4da53c79967b75b176fe1cf3
HenBarper <Benjaminitonio@gmail.com>
2023-02-08 09:57:31 -0800

    1 v1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+/*
+ * File 1-strncat.c
+ * Auth: Ben Harper
+ */
+
+#include "holberton.h"
+
+/**
+ * *_strncat - concatenate two strings
+ * @dest: destination value
+ * @src: source value
+ * @n: int var
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_length = 0;
+	int src_length;
+
+	for (src_length = 0 ; i < n && src[src_length] != '\0' ; src_length++)
+	{
+		dest[dest_length + src_length] = src[src_length];
+	}
+	dest[dest_length + src_length] = '\0';
+
+	return (dest);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -6,4 +6,5 @@
 int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
+char *_strncat(cahr *dest, char *src, int n);
 #endif
