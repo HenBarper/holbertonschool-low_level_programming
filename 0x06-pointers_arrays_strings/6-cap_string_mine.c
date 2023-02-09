b1135b64b388a3839a9e08901847fae8b5de8f6e
HenBarper <Benjaminitonio@gmail.com>
2023-02-09 09:38:27 -0800

    6 v8

--- 0x06-pointers_arrays_strings/6-cap_string_mine.c
+++ 0x06-pointers_arrays_strings/6-cap_string_mine.c
@@ -11,17 +11,17 @@
  * Return: nothing
  */
 
-char *cap_string(char *c)
+char *cap_string(char *s)
 {
 	int length;
 	int counter;
 	int i;
 	int torf = 0;
-	char prevCh = c[0];
+	char prevCh = s[0];
 	char sps[7] = { ' ', '\t', '\n', ',', ';', '.', '!' };
 	char sps2[6] = { '?', '"', '(', ')', '{', '}'};
 
-	while (c[length] != '\0')
+	while (s[length] != '\0')
 	{
 		length++;
 	}
@@ -31,7 +31,7 @@ char *cap_string(char *c)
 		torf = 0;
 		if (counter > 0)
 		{
-			prevCh = c[counter - 1];
+			prevCh = s[counter - 1];
 		}
 		for (i = 0 ; i < 7 ; i++)
 		{
@@ -45,12 +45,12 @@ char *cap_string(char *c)
 		}
 		if (torf == 1)
 		{
-			if ((c[counter]) > 96 && (c[counter]) < 123)
+			if ((s[counter]) > 96 && (s[counter]) < 123)
 			{
-				c[counter] = (c[counter]) - 32;
+				s[counter] = (s[counter]) - 32;
 			}
 		}
 	}
 
-	return (c);
+	return (s);
 }
