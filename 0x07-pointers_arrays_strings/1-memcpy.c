777c0fbf882a19d1b677cc64ca496dbaead7c79a
HenBarper <Benjaminitonio@gmail.com>
2023-02-10 10:02:29 -0800

    1 v4

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -15,6 +15,7 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	char *ptr = dest;
 	unsigned int i;
 
 	for (i = 0 ; i <= n ; i++)
@@ -22,5 +23,5 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 		dest[i] = src[i];
 	}
 
-	return (dest);
+	return (ptr);
 }
