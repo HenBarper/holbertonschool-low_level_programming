a28b5e67f33b3d263bb357ba51de3320ff044702
HenBarper <Benjaminitonio@gmail.com>
2023-02-10 10:21:46 -0800

    1 v9 copy

--- 0x07-pointers_arrays_strings/1-memcpy_mine.c
+++ 0x07-pointers_arrays_strings/1-memcpy_mine.c
@@ -15,13 +15,12 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *start = dest;
+	unsigned int i;
 
-	while (n--)
+	for (i = 0 ; i <= n ; i++)
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		dest[i] = src[i];
 	}
-	return (start);
+
+	return (dest);
 }
