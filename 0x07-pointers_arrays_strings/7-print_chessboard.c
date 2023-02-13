bc89984e8d6020699934eca4cc46454fc206afb4
HenBarper <Benjaminitonio@gmail.com>
2023-02-13 12:28:14 -0800

    7 v1

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,27 @@
+/*
+ * File 7-print_chessboard.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * print_chessboard - print the chessboard
+ * @a: string
+ * Return: nothing
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int row;
+	int col;
+
+	for (row = 0 ; row < 8 ; row ++)
+	{
+		for (col = 0 ; col < 8 ; col++)
+		{
+			_putchar(a[row][col]);
+		}
+		_putchar('\n');
+	}
+}
