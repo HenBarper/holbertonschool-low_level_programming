3bf2f0da2213d7e7dd6b3cb0b33653a1be0b2d2a
HenBarper <Benjaminitonio@gmail.com>
2023-02-13 13:16:54 -0800

    8 v1

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,34 @@
+/*
+ * File 8-print_diagsums.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * print_diagsums - print the sum of 2 diagonals of a square matrix of intergers
+ * @a: array
+ * @size: int
+ * Return: nothing
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int maxSize;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	maxSize = size * size;
+
+	for (i = 0 ; i < maxSize ; i += size + 1)
+	{
+		sum1 += a[i];
+	}
+
+	for (i = size - 1 ; i < maxSize - 1 ; i += size - 1)
+	{
+		sum2 += a[i];
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
