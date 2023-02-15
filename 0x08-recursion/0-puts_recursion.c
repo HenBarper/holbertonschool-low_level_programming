8cb9be8f76dfaf775cd7a95ff2abaa9581336149
HenBarper <Benjaminitonio@gmail.com>
2023-02-15 11:25:59 -0800

    0x08 set up

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+/*
+ * File 8-print_diagsums.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print a string followed by new line
+ * @s: string
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+void _puts_recursion(char *s);void _puts_recursion(char *s);
+
+#endif
