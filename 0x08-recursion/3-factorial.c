eb4e6af8c1b50c12775668b5efbb2e7fb048231b
HenBarper <Benjaminitonio@gmail.com>
2023-02-15 12:56:00 -0800

    3 v1

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+/*
+ * File 3-factorial.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * factorial - returns factorial of number
+ * @n: number
+ * Return: fact of number
+ */
+
+int factorial(int n)
+{
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
