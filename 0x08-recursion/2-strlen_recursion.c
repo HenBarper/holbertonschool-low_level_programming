6f8b1aae3a3762abb019714bd21579a96c3e6ff1
HenBarper <Benjaminitonio@gmail.com>
2023-02-15 12:52:05 -0800

    2 v1

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+/*
+ * File 2-strlen.recursion.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - return length of string
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if(*s)
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
