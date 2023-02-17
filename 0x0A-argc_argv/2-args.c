d3c98ac0ecb67f64527ca737e00a4597647ebce9
HenBarper <Benjaminitonio@gmail.com>
2023-02-17 11:48:31 -0800

    2 v1

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - print its name
+ * @argc: argument count
+ * @argv: argument vector for values
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	while (argc--)
+	{
+		printf("%s\n", *argv++);
+	}
+	return (0);
+}
