1c3172fa66c42a372a611246d7d6c3de66f18092
HenBarper <Benjaminitonio@gmail.com>
2023-02-19 09:42:31 -0800

    0x0B init

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+/*
+ * File 0-create.array.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+
+/**
+ * *create_array - create ar of chars, init w/ specfc char
+ * @size - bytes
+ * @c - char
+ * Return: NULL or ptr to ar
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return ('\0');
+	}
+}
