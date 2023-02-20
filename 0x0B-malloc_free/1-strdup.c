64f3bbe3f2436a851712db47a88985b8262cbd81
HenBarper <Benjaminitonio@gmail.com>
2023-02-20 10:04:19 -0800

    1 v1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,41 @@
+/*
+ * File 1-strdup.c
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *_strdup - ret ptr to newly allocated mem space
+ * @str: string
+ * Return: NULL or ptr to dupe string
+ */
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int n = 0;
+	char *ar;
+
+	while (str[i] != '\0')
+	{
+		i++;
+		n++;
+	}
+	n++;
+
+	ar = malloc(n * sizeof(char));
+	if (ar == '\0')
+	{
+		return ('\0');
+	}
+
+	for (i = 0 ; i < n ; i++)
+	{
+		ar[i] = str[i];
+	}
+
+	return (ar);
+}
