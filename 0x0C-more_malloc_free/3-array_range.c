7f08be7ac6ab94dfc64209aaf234820016f12ff6
HenBarper <Benjaminitonio@gmail.com>
2023-02-23 10:29:34 -0800

    3 v1

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+/*
+ * File 3-array_range
+ * Auth: Ben Harper
+ */
+
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * *array_range - create an arr of ints
+ * @min: min size
+ * @max: max size
+ * Return: ptr to new arr
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int len;
+	int i;
+
+	if (min > max)
+		return (NULL);
+
+	len = (max - min);
+
+	arr = malloc((len + 1) * sizeof(int));
+
+	for (i = 0; i <= len ; i++)
+	{
+		*(arr + i) = min + i;
+	}
+
+	return (arr);
+}
