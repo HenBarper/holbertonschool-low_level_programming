ed51a3fd88dee5b469dc1147c510d67fef1f2ac9
HenBarper <Benjaminitonio@gmail.com>
2023-03-01 10:22:34 -0800

    0 v2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: Data structure for a dog
+ */
+
 struct dog
 {
 	char *name;
