7aaf75159c060ad09ebcd3bf3e33a8f500132d54
HenBarper <Benjaminitonio@gmail.com>
2023-03-01 11:36:18 -0800

    4 v2

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -18,10 +18,35 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *doggo;
+	char *new_name;
+	char *new_owner;
+	int name_len;
+	int owner_len;
+	int i;
 
 	doggo = malloc(sizeof(dog_t));
 	if (doggo == NULL)
 		return (NULL);
+
+	while (name[name_len])
+		name_len++;
+	while (owner[owner_len])
+		owner_len++;
+	
+	new_name = malloc(name_len + 1);
+	if (new_name == NULL)
+		return (NULL);
+	for (i = 0 ; name[i] ; i++)
+		new_name[i] = name[i];
+	new_name[i] = '\0';
+
+	new_owner = malloc(owner_len + 1);
+	if (new_owner == NULL)
+		return (NULL);
+	for (i = 0 ; owner[i] ; i++)
+		new_owner[i] = owner[i];
+	new_owner[i] = '\0';
+
 	doggo->name = name;
 	doggo->age = age;
 	doggo->owner = owner;
