6910d179ba3200cd612dd2008973762e8140427b
HenBarper <Benjaminitonio@gmail.com>
2023-03-02 17:42:20 -0800

    0x0F init 2

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-print_name.c
+ * Author: Ben Harper
+ */
+
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @*name: ptr to name string
+ * @*f: ptr to f function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef HEADER_H
+#define HEADER_H
+void print_name(char *name, void (*f)(char *));
+#endif
