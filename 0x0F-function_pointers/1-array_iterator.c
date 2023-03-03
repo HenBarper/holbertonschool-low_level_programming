3585508568edfb7bdbd8f95e6378bd7998660eaf
HenBarper <Benjaminitonio@gmail.com>
2023-03-02 18:33:02 -0800

    1 v1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,26 @@
+/*
+ * File: 1-array_iterator
+ * Author: Ben Harper
+ */
+
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - execute a func as param on els of arr
+ * @array: int ptr
+ * @size: size_t
+ * @action: func ptr
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		action(array[i]);
+	}
+}
