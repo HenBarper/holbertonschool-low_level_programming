8fc53157c29ef10676e091525cae8641aeca2f98
HenBarper <Benjaminitonio@gmail.com>
2023-03-02 18:17:47 -0800

    0 v2

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -9,14 +9,15 @@
 
 /**
  * print_name - prints a name
- * @*name: ptr to name string
- * @*f: ptr to f function
+ * @name: ptr to name string
+ * @f: ptr to f function
  * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	void (*printit)(char *);
+
 	printit = f;
 	f(name);
 }
