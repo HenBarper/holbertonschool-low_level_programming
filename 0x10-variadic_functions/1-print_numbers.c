26a7b2b32ae9fbc64f3f5c5378a71b8765131919
HenBarper <Benjaminitonio@gmail.com>
2023-03-06 09:33:38 -0800

    1 v1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,45 @@
+/*
+ * File: 1-print_numbers.c
+ * Auth: Ben Harper
+ */
+
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print nums + new line
+ * @n: number of params
+ * @...: other params
+ * @separator: print between nums
+ * Return: nothing
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list prms;
+	int i;
+	int num;
+
+	va_start(prms, n);
+
+	for (i = 0 ; i < n ; i++)
+	{
+		num = va_arg(prms, int);
+		if (separator != NULL && i < (n - 1))
+		{
+			printf("%d%s", num, separator);
+		}
+		else if (separator == NULL && i < (n - 1))
+		{
+			printf("%d ", num);
+		}
+		else
+		{
+			printf("%d", num);
+		}
+	}
+	printf("\n");
+
+	va_end(prms);
+}
