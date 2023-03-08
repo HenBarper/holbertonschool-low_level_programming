a2f79d9df54ac0043db462f549d85420832f5331
HenBarper <Benjaminitonio@gmail.com>
2023-03-08 14:20:20 -0800

    dsajkj

--- printpyth.c
+++ printpyth.c
@@ -1,30 +1,57 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <stdarg.h>
 
 int _putchar(char c);
+void _puts(char *str);
 int _printf(const char *format, ...);
 
 int main() 
 {
-	  _printf("nuts");
+	int nuts = 5;
+	_printf("there's %i nuts", nuts);
 }
 
 int _putchar(char c)
 {
-	    return (write(1, &c, 1));
+	return (write(1, &c, 1));
+}
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
 }
 
 int _printf(const char *format, ...)
 {
 	int i = 0;
 
+	va_list valist;
+	va_start(valist, format);
 	while (format[i] != '\0')
 	{
-		_putchar(format[i]);
+		if (format[i] == '%')
+		{
+			i++;
+			if(format[i] == 'i')
+			{
+				printf(va_arg(valist, int));
+				//_putchar(va_arg(valist, int));
+				//puts(va_arg(valist, int));
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+		}
 		i++;
 	}
-	//va_list valist;
-        //va_start(valist, format);
-	//va_arg(valist, int));
-	//va_end(valist);
+	va_end(valist);
 }
