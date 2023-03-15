baa983447f446262e2666f7053f63a0026632795
HenBarper <Benjaminitonio@gmail.com>
2023-03-15 10:10:16 -0700

    1 v2

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,9 +15,11 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-} list_t;
+} list_t
 
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+
 int _putchar(char c);
 
 #endif
