08d6c200c3ec263b685e269e479b942f5130c0f5
HenBarper <Benjaminitonio@gmail.com>
2023-03-15 09:03:41 -0700

    0 v1

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,4 +8,20 @@
 
 size_t print_list(const list_t *h)
 {
+	size_t i = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
