d9179a01bbc4559ca4526328756cbbb13d1bb1fa
HenBarper <Benjaminitonio@gmail.com>
2023-03-16 13:09:02 -0700

    5 v1

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - free the list
+ * @head: head node
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = head;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+	head = NULL;
+}
