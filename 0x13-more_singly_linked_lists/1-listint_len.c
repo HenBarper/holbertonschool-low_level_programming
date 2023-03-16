8c2159d7d01e802cda38f80270aca605b5ff0f29
HenBarper <Benjaminitonio@gmail.com>
2023-03-16 11:37:07 -0700

    1 v1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * listint_len - return length of list
+ * @h: list var
+ * Return: the number of nodes
+ */
+
+size_t listint_len(const listint_t *h);
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
