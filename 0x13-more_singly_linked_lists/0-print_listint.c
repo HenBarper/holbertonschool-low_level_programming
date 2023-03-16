d4a2c20915e09e59961d3f80b170daadd671352c
HenBarper <Benjaminitonio@gmail.com>
2023-03-16 10:37:13 -0700

    0 v1

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+/**
+ * print_listint - print elements of list
+ * @h: list to print
+ * Return: the number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		printf("%i\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
