ad75335cc74d4fa2c87a0af6661f748f2ee3890e
HenBarper <Benjaminitonio@gmail.com>
2023-03-16 13:13:06 -0700

    5 v2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,11 +12,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = head;
+	temp = *head;
 	while (head != NULL)
 	{
-		temp = head;
-		head = head->next;
+		temp = *head;
+		*head = (*head)->next;
 		free(temp);
 	}
 	*head = NULL;
