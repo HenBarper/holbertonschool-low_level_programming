106b87c51b2eab39e9e6e2d60d48875abb005665
HenBarper <Benjaminitonio@gmail.com>
2023-03-17 09:01:03 -0700

    9 v4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,8 +43,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->next = temp->next;
 	temp->next = new;
-	if (new == NULL)
-		return (NULL);
-
 	return (new);
 }
