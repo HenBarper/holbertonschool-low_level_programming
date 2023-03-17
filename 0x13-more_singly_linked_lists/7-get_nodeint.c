43981f650fda8b2c1e62eaddb29c99994441c766
HenBarper <Benjaminitonio@gmail.com>
2023-03-17 07:55:28 -0700

    7 v3

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -18,6 +18,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	for (i = 0; i < index ; i++)
 	{
 		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
 	}
 	return (temp);
 }
