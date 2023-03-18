5df6943a5308e944b07562163df0cc441ce60769
HenBarper <Benjaminitonio@gmail.com>
2023-03-18 06:36:19 -0700

    0x14 init

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - convert binary # to uint
+ * @b: ptr to string of 0s and 1s
+ * Return: converted number or 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	if (b == NULL)
+	{
+		return (0);
+	}
+}
