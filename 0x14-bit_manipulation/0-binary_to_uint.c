da7df3d17579135fe61fa6205139a1d7ad2f47d9
HenBarper <Benjaminitonio@gmail.com>
2023-03-18 06:53:15 -0700

    0 v1

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,8 +9,24 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int len = 0;
+	unsigned int base = 1, result = 0;
+
 	if (b == NULL)
-	{
 		return (0);
+
+	while (b[len])
+		len++;
+
+	while (len)
+	{
+		if (b[len - 1] != '0' && b[len - 1] != '1')
+			return (0);
+
+		if (b[len - 1] == '1')
+			result += base;
+		base *= 2;
+		len--;
 	}
+	return  (result);
 }
