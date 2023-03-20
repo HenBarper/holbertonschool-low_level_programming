09934de45c71ebe04cbf71dee1a5ae717f01281f
HenBarper <Benjaminitonio@gmail.com>
2023-03-20 15:40:18 -0700

    1 v3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,6 +21,7 @@ int create_file(const char *filename, char *text_content)
 	while (text_content[length] != '\0')
 		length++;
 
+	length--;
 	fdwrite = write(fdopen, text_content, length);
 	close(fdopen);
 	if (fdwrite < 0)
