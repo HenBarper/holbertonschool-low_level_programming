fd71c3f890792283f90f55a55d7655a80c182102
HenBarper <Benjaminitonio@gmail.com>
2023-03-20 14:07:33 -0700

    0 v1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -7,6 +7,32 @@
  * Return: the actual number of letters it could read and print
  */
 
-size_t read_textfile(const char *filename, size_t letters)
+ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, rd, wd;
+	char *buff = malloc(sizeof(char) * letters);
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	fd = open(filename, O_RDWR);
+	if (fd == -1)
+	{
+		return (0);
+	}
+	rd = read(fd, buff, letters);
+	if (rd == -1)
+	{
+		return (0);
+	}
+	wd = write(STDOUT_FILENO, buff, rd);
+	if (wd == -1)
+	{
+		return (0);
+	}
+	close(fd);
+	free(buff);
+	return (wd);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,8 +2,14 @@
 #define MAIN_H
 
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/uio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 int _putchar(char c)int _putchar(char c);
-size_t read_textfile(const char *filename, size_t letters);
+ssize_t read_textfile(const char *filename, size_t letters);
 
 #endif
