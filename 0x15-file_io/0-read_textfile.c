910ad246bfe285daf894dcf2b74c92218d9f2003
HenBarper <Benjaminitonio@gmail.com>
2023-03-20 13:11:05 -0700

    0x15 init

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * read_textfile - read a text file and print to POSIX std out
+ * @filename: the name of the file
+ * @letters: number of letters to read or print
+ * Return: the actual number of letters it could read and print
+ */
+
+size_t read_textfile(const char *filename, size_t letters)
+{
+}
