1fd1ee076c7eb808cade365d923084db0baab063
HenBarper <Benjaminitonio@gmail.com>
2023-03-22 13:24:58 -0700

    5 v1

---
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint - return nth node of list
+ * @head: the list
+ * @index: the nth node
+ * Return: The node gotten
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	listint_t *temp;
+	unsigned int i;
+
+	temp = head;
+	for (i = 0; i < index ; i++)
+	{
+		temp = temp->next;
+		if (temp == NULL)
+			return (NULL);
+	}
+	return (temp);
+}
