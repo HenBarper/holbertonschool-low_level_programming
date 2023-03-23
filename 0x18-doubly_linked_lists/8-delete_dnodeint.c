222468ac2832496d87ef6a6f955a186c22866f8e
HenBarper <Benjaminitonio@gmail.com>
2023-03-23 12:11:28 -0700

    8 v2

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -10,35 +10,41 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *temp, *dnode;
-	unsigned int i = 1;
+	unsigned int i = 0;
 
-	if (*head == NULL)
+	if (*head == NULL || head == NULL)
 		return (-1);
 
 	temp = *head;
 	if (index == 0)
 	{
-		*head = (*head)->next;
-		(*head)->prev = NULL;
+		if (temp->next)
+		{
+			*head = (*head)->next;
+			(*head)->prev = NULL;
+		}
+		else
+			*head = NULL;
+
 		free(temp);
+		return (1);
 	}
-	else
+	while (temp && i < index - 1)
 	{
-		while (i < index)
-		{
-			temp = temp->next;
-			if (temp == NULL)
-				return (-1);
-			i++;
-		}
-		dnode = temp->next;
-		temp->next = dnode->next;
-		if (dnode->next)
-		{
-			dnode = dnode->next;
-			dnode->prev = temp;
-		}
-		free(dnode);
+		temp = temp->next;
+		i++;
 	}
+
+	if (temp == NULL)
+		return (-1);
+
+	dnode = temp->next;
+	temp->next = dnode->next;
+	if (dnode->next)
+	{
+		dnode = dnode->next;
+		dnode->prev = temp;
+	}
+	free(dnode);
 	return (1);
 }
