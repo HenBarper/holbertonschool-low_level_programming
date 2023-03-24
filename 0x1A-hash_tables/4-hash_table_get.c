5a23ff38e239a11bc3deaa2dfb507853c19dda9e
HenBarper <Benjaminitonio@gmail.com>
2023-03-24 10:35:11 -0700

    4 v2

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	char *value;
 
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	value = ht->array[index]->value;
 
