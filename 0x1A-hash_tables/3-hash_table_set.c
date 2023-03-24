021a5832e4d59ad684909a2fd8e51fa8185973cf
HenBarper <Benjaminitonio@gmail.com>
2023-03-24 10:20:46 -0700

    3 v3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,6 +3,7 @@
 /**
  * free_node - Free a node.
  * @node: Node to free.
+ *
  * Return: Void.
  */
 void free_node(hash_node_t *node)
@@ -13,19 +14,18 @@ void free_node(hash_node_t *node)
 }
 
 /**
- * hash_table_set - add an element to hash table
- * @ht: the hash table
- * @key: the key
- * @value: val to add
- * Return: index of key value pair
+ * hash_table_set - Set a value in the hash table.
+ * @ht: Hash table.
+ * @key: Key to be indexed.
+ * @value: Value to set in the hash table.
+ * Return: 1 if works, 0 if doesn't.
  */
-
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node, *current;
 	unsigned long int index;
+	hash_node_t *new_node, *current;
 
-	if (key == NULL || value == NULL)
+	if (strcmp(key, "") == 0 || key == NULL || ht == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
@@ -46,8 +46,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free_node(current);
 			return (1);
 		}
-		while (current->next != NULL && strcmp(current->key, key) != 0)
-			current = current->next;
+		while (current->next != NULL && strcmp(current->next->key, key) != 0)
+		{ current = current->next;
+		}
 		if (strcmp(current->key, key) == 0)
 		{
 			new_node->next = current->next->next;
