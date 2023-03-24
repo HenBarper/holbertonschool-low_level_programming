088fcee82d49320646eb1457d803d391e5e580e6
HenBarper <Benjaminitonio@gmail.com>
2023-03-24 10:33:09 -0700

    4 v1

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retieve value associated with key
+ * @key: the key
+ * @ht: the hash table
+ * Return: value associated with element
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	char *value;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	value = ht->array[index]->value;
+
+	return (value);
+}
