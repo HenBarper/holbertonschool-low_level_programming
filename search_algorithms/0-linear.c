000f98d36f58021bef691af61248ac1820464335
henbarper <benjaminitonio@gmail.com>
2023-12-12 12:58:41 -0600

    0 v4

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -9,20 +9,20 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i = 0;
-	
-    if(array == NULL || size == 0)
-    {
-        return(-1);
-    }
+	size_t i = 0;
 
-    for(i = 0; i < size; i++)
-    {
-        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-        if(array[i] == value)
-        {
-	        return(i);
-	    }
+	if(array == NULL || size == 0)
+	{
+	    return(-1);
 	}
+
+	for(i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if(array[i] == value)
+		{
+			return(i);
+		}
+		}
 	return(-1);
 }
