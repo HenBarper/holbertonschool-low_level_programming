feda04f202b26f51b83c4679183a1aeb10e1f01d
henbarper <benjaminitonio@gmail.com>
2023-12-12 12:53:36 -0600

    0 v1

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - do a linear search
+ * @array: pointer to first element of array
+ * @size: size of array
+ * @value: value to search for
+ * Return: first index of value when located
+ */
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+    if(array == NULL || size == 0)
+    {
+        return(-1);
+    }
+
+    for(i = 0; i < size; i++)
+    {
+        printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+        if(array[i] == value)
+        {
+            return(i);
+        }
+    }
+    /* while(array)
+    {
+        printf("current index: %d\n", i);
+        if(*array == value)
+        {
+            return(i);
+        }
+        array++;
+        i++;
+    } */
+    return(-1);
+}
